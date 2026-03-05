7978f5c2d375ba59f7948cc8e960c21088f756c2
Sakib Faturrahman <sakibfaturrahman92@gmail.com>
2026-03-05 14:23:27 +0700

    update headers

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,10 +1,12 @@
 const UserAgent = require("user-agents");
 
-const getHeaders = () => {
+const getHeaders = (source) => {
   return {
-    "User-Agent": new UserAgent().toString(),
-    Referer: "https://09.shinigami.asia/",
-    "Accept-Language": "en-US,en;q=0.9",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Referer': 'https://komiku.org/', // WAJIB ADA
+    'Origin': 'https://komiku.org'
   };
 };
 
